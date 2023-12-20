c1aac6ede00796c707b930f5658c7b1e76e8be71
Hafiz Caniago <hafizcode02@gmail.com>
2023-12-20 16:17:57 +0700

    feat: separate endpoint for brondolan and bongkahan

--- main.py
+++ main.py
@@ -40,12 +40,11 @@ def index():
         }
     }), 200
 
-# Route Predict
-@app.route('/predict', methods=['POST'])
-def predict():
+# Route Predict Bongkahan
+@app.route('/predict-bongkahan', methods=['POST'])
+def predictBongkahan():
     if request.method == 'POST':
         reqImage = request.files['image'] # Get Image Files
-        reqType = request.form['type'] # Get Type of Scan : bongkahan or brondolan
         if reqImage and allowed_file(reqImage.filename):
             filename = secure_filename(reqImage.filename)
             reqImage.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
@@ -58,56 +57,88 @@ def predict():
             detect_non_sawit = model_non_sawit.predict(x)[0][0] # Predict Non Sawit
             # Check if Sawit
             if(detect_non_sawit > 0.5):
-                # Check if Bongkahan
-                if(reqType == "bongkahan"):
-                    detect_bongkahan = model_bongkahan.predict(x)[0][0] # Predict Bongkahan
+                detect_bongkahan = model_bongkahan.predict(x)[0][0] # Predict Bongkahan
                     # if sawit bongkahan mentah
-                    if(detect_bongkahan > 0.5):
-                        return jsonify({
-                            'status': {
-                                'code': 200,
-                                'message': 'Success predicting',
-                                'data': { 'classType': 'Bongkahan Sawit Mentah', 'precentase': int(detect_bongkahan * 100) },
-                            }
-                        }), 200
+                if(detect_bongkahan > 0.5):
+                    return jsonify({
+                        'status': {
+                            'code': 200,
+                            'message': 'Success predicting',
+                            'data': { 'classType': 'Bongkahan Sawit Mentah', 'precentase': int(detect_bongkahan * 100) },
+                        }
+                    }), 200
                     # if sawit bongkahan matang
-                    else:
-                        return jsonify({
-                            'status': {
-                                'code': 200,
-                                'message': 'Success predicting',
-                                'data': { 'classType': 'Bongkahan Sawit Matang', 'precentase': (100 - int(detect_bongkahan * 100)) },
-                            }
-                        }), 200
-                # Check if brondolan
-                elif(reqType == "brondolan"):
-                    detect_brondolan = model_brondolan.predict(x)[0][0] # predict brondolan
-                    # if sawit brondolan mentah
-                    if(detect_brondolan > 0.5):
-                        return jsonify({
-                            'status': {
-                                'code': 200,
-                                'message': 'Success predicting',
-                                'data': { 'classType': 'Brondolan Sawit Mentah', 'precentase': int(detect_brondolan * 100) },
-                            }
-                        }), 200
-                    # if sawit brondolan matang
-                    else:
-                        return jsonify({
-                            'status': {
-                                'code': 200,
-                                'message': 'Success predicting',
-                                'data': { 'classType': 'Brondolan Sawit Matang', 'precentase': (100 - int(detect_brondolan * 100)) },
-                            }
-                        }), 200
-                # Check if not bongkahan or brondolan
                 else:
                     return jsonify({
                         'status': {
-                            'code': 400,
-                            'message': 'Invalid type of scan. Please choose bongkahan or brondolan.'
+                            'code': 200,
+                            'message': 'Success predicting',
+                            'data': { 'classType': 'Bongkahan Sawit Matang', 'precentase': (100 - int(detect_bongkahan * 100)) },
                         }
-                    }), 400
+                    }), 200
+                # Check if not bongkahan or brondolan
+            # non sawit
+            else:
+                return jsonify({
+                    'status': {
+                        'code': 200,
+                        'message': 'Success predicting',
+                        'data': { 'classType': 'Bukan Sawit', 'precentase': (100 - int(detect_non_sawit * 100)) },
+                    }
+                }), 200
+        # if not allowed file
+        else:
+            return jsonify({
+                'status': {
+                    'code': 400,
+                    'message': 'Invalid file format. Please upload a JPG, JPEG, or PNG image.'
+                }
+            }), 400
+    # if not POST method
+    else:
+        return jsonify({
+            'status': {
+                'code': 405,
+                'message': 'Method not allowed'
+            }
+        }), 405
+    
+# Route Predict Brondolan
+@app.route('/predict-brondolan', methods=['POST'])
+def predictBrondolan():
+    if request.method == 'POST':
+        reqImage = request.files['image'] # Get Image Files
+        if reqImage and allowed_file(reqImage.filename):
+            filename = secure_filename(reqImage.filename)
+            reqImage.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            img = Image.open(image_path).convert("RGB")
+            img = img.resize((150, 150))
+            x = tf_image.img_to_array(img)
+            x = np.expand_dims(x, axis=0)
+            x = x / 255
+            detect_non_sawit = model_non_sawit.predict(x)[0][0] # Predict Non Sawit
+            # Check if Sawit
+            if(detect_non_sawit > 0.5):
+                detect_brondolan = model_brondolan.predict(x)[0][0] # predict brondolan
+                # if sawit brondolan mentah
+                if(detect_brondolan > 0.5):
+                    return jsonify({
+                        'status': {
+                            'code': 200,
+                            'message': 'Success predicting',
+                            'data': { 'classType': 'Brondolan Sawit Mentah', 'precentase': int(detect_brondolan * 100) },
+                        }
+                    }), 200
+                    # if sawit brondolan matang
+                else:
+                    return jsonify({
+                        'status': {
+                            'code': 200,
+                            'message': 'Success predicting',
+                            'data': { 'classType': 'Brondolan Sawit Matang', 'precentase': (100 - int(detect_brondolan * 100)) },
+                        }
+                    }), 200
             # non sawit
             else:
                 return jsonify({
